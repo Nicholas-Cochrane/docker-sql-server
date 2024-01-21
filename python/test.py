904e1da7ae2d7ea8aaa728137bc14e2bcd27d3d7
Nicholas-Cochrane <nicholascochrane25@gmail.com>
2024-01-20 21:15:26 -0800

    more server progress

--- python/test.py
+++ python/test.py
@@ -1,30 +1,31 @@
 from aiohttp import web
 import logging
 
-def get_module_logger(mod_name):
-    """
-    To use this, do logger = get_module_logger(__name__)
-    """
-    logger = logging.getLogger(mod_name)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-        '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
-    return logger
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+    
 
 if __name__ == "__main__":
-    get_module_logger(__name__).info("HELLO WORLD!")
+    logger.info("HELLO WORLD!")
 
 async def getResponse(request):
     return web.Response(text="This URL is primaraly for POST. This is not a page.")
 
 async def processSensorReadings(request: web.Request) -> web.Response:
     """Process JSON from Airgradient """
+    sensor_id = request.match_info["sensor_id"]
+    logger.info(sensor_id)
+    logger.info("this is line 22")
     try:
         readings = await request.json()
-        get_module_logger(__name__).info(readings)
+
+        for k,v in readings.items():
+            logger.info(k)
+            logger.info(v)
     except ValidationError:
         logger.error("Bad request from %s: %s", sensor_id, await request.text())
         return web.Response(status=400)
